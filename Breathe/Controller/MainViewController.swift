407dafb8b6ab791911966d36cdb078bcafb04cbc
Jose Carlos <josealarconchacon@gmail.com>
2020-11-24 20:08:03 -0500

    set up small view

--- Breathe/Controller/MainViewController.swift
+++ Breathe/Controller/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import BonsaiController
 
 class MainViewController: UIViewController {
     
@@ -43,7 +44,16 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let smallView = storyboard!.instantiateViewController(identifier: "SmallView")
+        smallView.transitioningDelegate = self
+        smallView.modalPresentationStyle = .custom
         present(smallView, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.destination is YourViewController {
+            segue.destination.transitioningDelegate = self
+            segue.destination.modalPresentationStyle = .custom
+//        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -52,3 +62,11 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 }
 
 
+extension MainViewController: BonsaiControllerDelegate {
+    func frameOfPresentedView(in containerViewFrame: CGRect) -> CGRect {
+        return CGRect(origin: CGPoint(x: 0, y: containerViewFrame.height / 1.3), size: CGSize(width: containerViewFrame.width, height: containerViewFrame.height / (3/4)))
+        }
+        func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+            return BonsaiController(fromDirection: .bottom, blurEffectStyle: .light, presentedViewController: presented, delegate: self)
+        }
+}
---
+++ Breathe/Controller/SmallView.swift
@@ -0,0 +1,18 @@
+//
+//  SmallView.swift
+//  Breathe
+//
+//  Created by Jose Alarcon Chacon on 11/24/20.
+//
+
+import UIKit
+
+class SmallView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

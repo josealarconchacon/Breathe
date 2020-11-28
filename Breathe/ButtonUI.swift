210c43d5391e6155bca6c0f546b5f38c1fddf4eb
Jose Carlos <josealarconchacon@gmail.com>
2020-11-27 19:57:50 -0500

    set up uislider

---
+++ Breathe/ButtonUI.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonUI.swift
+//  Breathe
+//
+//  Created by Jose Alarcon Chacon on 11/27/20.
+//
+
+import Foundation
--- Breathe/Controller/SmallView.swift
+++ Breathe/Controller/SmallView.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class SmallView: UIViewController {
 
+    @IBOutlet weak var minutesLable: UILabel!
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBOutlet weak var startButtonOutlet: UIButton!
+    
+    var minutes = 5
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func setTime(_ sender: UISlider) {
+        minutes = Int(sender.value)
+        minutesLable.text = String(minutes)
+    }
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(starBreathing), userInfo: nil, repeats: true)
+    }
+    
+    @objc func starBreathing() {
+        minutes -= 1
+        minutesLable.text = String(minutes)
+        
+        if(minutes == 0) {
+            timer.invalidate()
+        }
+    }
 }

f0403e2e825782e3477d26f40d32d1b280d52a52
Jose Carlos <josealarconchacon@gmail.com>
2020-12-02 14:10:26 -0500

    Update nutton ui

--- Breathe/ButtonUI.swift
+++ Breathe/ButtonUI.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIButton {
+    func button_ui() {
+        layer.backgroundColor = UIColor.clear.cgColor
+        layer.cornerRadius = 20
+        layer.borderWidth = 0.5
+        layer.borderColor = UIColor.black.cgColor
+    }
+}
